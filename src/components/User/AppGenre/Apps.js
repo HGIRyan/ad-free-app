e0cc71adcb188a97f7a4f5a257b1026feec6a0b0
Ryan Hutchison <ryanzhutch@gmail.com>
2019-01-10 15:06:04 -0700

    save

--- src/components/User/AppGenre/Apps.js
+++ src/components/User/AppGenre/Apps.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 // Imports
 import DevChoice from './devChoice'
 import SuggestedApps from './SuggestedApps'
+import { Link } from 'react-router-dom'
 
 export default class Apps extends Component {
     constructor() {
@@ -23,15 +24,21 @@ export default class Apps extends Component {
         return (
             <div>
                 {this.state.loaded ?
-                <div>
-                    Apps
-                    <DevChoice />
-                    <SuggestedApps />
-                </div>
-                :
-                <div>
-                    <img src='https://cdn.dribbble.com/users/891352/screenshots/3310131/bobotov_loader_002.gif' alt='Loading Gif' />
-                </div>}
+                    <div className='TodayViewPage'>
+                        <div className='GameHeader'>
+                            <h1>Apps</h1>
+                            <Link to='/user/settings'>
+                                <button><img src='https://static1.squarespace.com/static/5aca4db8d274cb14610bb2a3/t/5ae6590f575d1fd630f1b6d4/1525044987257/Ben+Arnon+profile+pic+%28circle+frame%29.png' alt='' className='headerImg' /></button>
+                            </Link>
+                            <hr/>
+                        </div>
+                        <DevChoice />
+                        <SuggestedApps />
+                    </div>
+                    :
+                    <div>
+                        <img src='https://cdn.dribbble.com/users/891352/screenshots/3310131/bobotov_loader_002.gif' alt='Loading Gif' />
+                    </div>}
             </div>
         )
     }
--- src/components/User/Games/Games.js
+++ src/components/User/Games/Games.js
@@ -35,6 +35,7 @@ export default class Games extends Component {
                             <Link to='/user/settings'>
                                 <button><img src='https://static1.squarespace.com/static/5aca4db8d274cb14610bb2a3/t/5ae6590f575d1fd630f1b6d4/1525044987257/Ben+Arnon+profile+pic+%28circle+frame%29.png' alt='' className='headerImg' /></button>
                             </Link>
+                            <hr/>
                         </div>
                         <hr/>
                         <NewApp />
